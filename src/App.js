74f75356348f46e25e2b7c2944377e890bdebe05
dmlfahfn <dmlfahfn1@hotmail.se>
2021-09-01 11:43:43 +0200

    shpwing the current date

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Moment from 'react-moment';
+import 'moment-timezone';
+import Calender from './Calender';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Calender />
+    )
+  }
+};
+
+export default App;
